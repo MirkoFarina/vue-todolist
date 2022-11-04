476f27e8dc39468548628811cbec392d26229380
MirkoFarina <MirkoFarina@outlook.com>
2022-11-04 14:08:10 +0100

    [add] remove action complete

--- js/app.js
+++ js/app.js
@@ -16,12 +16,18 @@ createApp ({
                     nome: 'Studiare React',
                     toDo: false
                 }
-            ]
+            ],
+            errorMsg: ''
         }
     },
     methods: {
         removeTask(index){
-            
+            this.errorMsg = '';
+            if(this.tasks[index].toDo) this.tasks.splice(index, 1);
+            else {
+                this.errorMsg = 'Attenzione! devi prima eseguire l\'azione';
+            }
         }
     }
 }).mount('#app')
+
